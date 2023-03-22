ab08f0fc93d3bb58863aac082a99fee31f005090
raburu11 <donraburu675@gmail.com>
2023-03-22 06:44:23 +0300

    fixing react router code

--- src/files/nav.js
+++ src/files/nav.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import { Link, useLocation } from 'react-router-dom'
+
 import styles from './nav.module.css';
 import myImage from './images/github.png'
 
 
-
-
-const Nav = () => {
+export default function Nav  ()  {
     return(
         <>
          <nav className={styles.navbar}>
             <div className={styles.logo}>
             <img src='' alt="LOGO" />
             </div>
+
             <ul className={styles.ul}>
-                <li className={styles.links}><a className={styles.a} href='/'>Home</a></li>
-                <li className={styles.links}><a className={styles.a} href='/'>About</a></li>
-                <li className={styles.links}><a className={styles.a} href='/'>Services</a></li>
-                <li className={styles.links}><a className={styles.a} href='/'>Projects</a></li>
-                <li className={styles.links}><a className={styles.a} href='/'>Contact</a></li>
+                    <li className={styles.links}>  <CustomLink className={styles.a} to='/'>Home</CustomLink>  </li>
+                    <li className={styles.links}> <CustomLink className={styles.a} to='/my-portfolio/src/files/about.js'>About</CustomLink>  </li>
+                    <li className={styles.links}>  <CustomLink className={styles.a} to='/services'>Services</CustomLink> </li>
+                    <li className={styles.links}> <CustomLink className={styles.a} to='/projects'>Projects</CustomLink> </li>
+                   <li className={styles.links}> <CustomLink className={styles.a} to='/'>Contact</CustomLink> </li>
+                
             </ul>
+
             <div className={styles.githubLink}>
                 <a  href='/'>
                 <img src={myImage} alt="GITHUB LINK" />
@@ -30,4 +32,14 @@ const Nav = () => {
     )
 }
 
-export default Nav;
+function CustomLink({ to, children, ...props }){
+    const location = useLocation()
+
+    return(
+        <li className={location.pathname === to ? "active" :""} >
+            <Link to={to}  {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
